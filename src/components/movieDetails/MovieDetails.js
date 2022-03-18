4787d71a0220a4a6e7a3010b6c8d3d2001f3857e
dmottabass29 <dmottabass@gmail.com>
2022-03-18 16:48:19 -0300

    adding loading  conditional renderization

--- src/components/movieDetails/MovieDetails.js
+++ src/components/movieDetails/MovieDetails.js
@@ -16,56 +16,63 @@ const MovieDetails = () => {
   useEffect(() => {
     dispatch(fetchAsyncMoviesDetails(imdbID));
     //clean previous movie/series details
-      return () => {
-        dispatch(removeSelectedMovieOrSeries())
-      }
+    return () => {
+      dispatch(removeSelectedMovieOrSeries());
+    };
   }, [dispatch, imdbID]);
 
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <div className="movie-title">{data.Title}</div>
-        <div className="movie-rating">
-          <span>
-            IMDB rating <i className="fa fa-star"></i> : {data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}{" "}
-          </span>
-          <span>
-            RunTime <i className="fa fa-film"></i> : {data.Runtime}{" "}
-          </span>
-          <span>
-            Year <i className="fa fa-year"></i> : {data.Year}{" "}
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+      {Object.keys(data).length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                IMDB rating <i className="fa fa-star"></i> : {data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i className="fa fa-thumbs-up"></i> :{" "}
+                {data.imdbVotes}{" "}
+              </span>
+              <span>
+                RunTime <i className="fa fa-film"></i> : {data.Runtime}{" "}
+              </span>
+              <span>
+                Year <i className="fa fa-year"></i> : {data.Year}{" "}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Genres</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Language</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Genres</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Language</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} alt={data.Title} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
